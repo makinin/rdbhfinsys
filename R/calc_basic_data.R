4ecf27d57bce712a445039be67bf1a9c299c3675
Marija Ninic <57001187+makinin@users.noreply.github.com>
2021-12-12 23:08:23 +0100

    Add files via upload

--- R/calc_basic_data.R
+++ R/calc_basic_data.R
@@ -23,29 +23,30 @@ lag_produksjonstabell <- function(df,
       funs <- identity
    }
    df %>%
-      dplyr::filter(indikator %in% filter_indikatorer) %>%
+      dplyr::filter(.data$indikator %in% filter_indikatorer) %>%
       tidyr::unite("kolonne_vars_samlet", kolonne_vars) %>%
       dplyr::group_by(across(c("institusjonskode_nyeste",
                     "institusjonsnavn_nyeste",
                     "kortnavn_nyeste",
                     "kolonne_vars_samlet",
                     "budsjettar"))) %>%
-      dplyr::summarise(dplyr::across("indikatorverdi", sum)) %>%
+      dplyr::summarise(dplyr::across(c("indikatorverdi", "indikatorendring"), sum)) %>%
       dplyr::ungroup() %>%
-      tidyr::pivot_wider(names_from = "kolonne_vars_samlet", values_from  = "indikatorverdi") %>%
+      tidyr::pivot_wider(names_from =
+      "kolonne_vars_samlet", values_from  = c("indikatorverdi", "indikatorendring") ) %>%
       dplyr::mutate(dplyr::across(-dplyr::matches("institusjon|navn|arstall"), funs))
 }
 
 
 #' Lager tabeller med produksjonsdata, tilsvarende dem i Blått hefte
 #' @description funksjon for å lage tabeller med grunnlagsdata
-#' @param arstall velg årstall
+#' @param df velg tibble gjerne output fra finsys_dbh_tidy
 #' @return a tibble
 #' @export
 #'
 
-finsys_produksjonstabeller <- function(arstall){
-   df <- finsys_dbh_tidy(arstall = arstall)
+finsys_produksjonstabeller <- function(df){
+
    tribble(~filter_indikatorer, ~kolonne_vars, ~funs,
            "studiepoeng", "kategori", NULL,
            "kandidater", c("kategori", "faktor"), NULL,
@@ -56,7 +57,7 @@ finsys_produksjonstabeller <- function(arstall){
 
    pmap(function(...) {
       df %>%
-         dplyr::filter(budsjettar == arstall + 2) %>%
+
 
          lag_produksjonstabell(...)
    })
@@ -67,22 +68,68 @@ finsys_produksjonstabeller <- function(arstall){
 #' @description budsjettendring tabeller fra api data
 #'
 #' @param df data set fra finsys_dbh_tidy
-#'
+#' @param .data a tibble
 #' @return
 #' @export
 #'
 #' @importFrom dplyr if_else
 #' @importFrom dplyr matches
-finsys_budsjettendringer <- function(df) {
+finsys_budsjettendringer <- function(df, .data) {
+   unntak <- .finsys_dbh_unntak()
+   indikatorsatser <- .finsys_dbh_indikatorsatser()
+   tilskuddsgrad <- .finsys_dbh_tilskuddsgrad()
+   prisjustering <- .finsys_dbh_prisjustering()
    df %>%
-      dplyr::group_by(budsjettar,
-               institusjonskode_nyeste,
-               institusjonsnavn_nyeste,
-               kortnavn_nyeste,
-               indikator) %>%
-      dplyr::summarise(budsjettendring_urund = sum(budsjettendring)) %>%
+   dplyr::anti_join(unntak,
+   by = c("institusjonskode",  "indikator")) %>%
+      # Legger til satser og beregner budsjettendring
+      #for indikatorer med åpen ramme
+      dplyr::left_join(indikatorsatser,
+      by = c("budsjettar", "indikator", "kategori")) %>%
+      dplyr::left_join(tilskuddsgrad,
+      by = c("institusjonskode",  "indikator")) %>%
+      dplyr::mutate(budsjettendring =
+      .data$indikatorendring *
+      .data$sats_rb *
+      tidyr::replace_na(.data$faktor, 1) *
+      tidyr::replace_na(.data$tilskuddsgrad, 1)) %>%
 
-      dplyr::mutate(budsjettendring = .round_afz(budsjettendring_urund, -3))
+      # Legger til historisk uttelling for indikatorer med lukket ramme
+      dplyr::left_join(dplyr::left_join
+      (dplyr::filter(uttelling_historisk,
+      .data$indikator %in% c("publisering", "BOA", "EU", "NFR")),
+      prisjustering, by = "budsjettar") %>%
+      dplyr::mutate(uttelling_prisjustert =
+      .data$uttelling / .data$arsdeflator) %>%
+      dplyr::select(.data$budsjettar,
+      .data$institusjonskode, .data$indikator, .data$uttelling_prisjustert),
+      by = c("budsjettar", "institusjonskode", "indikator")) %>%
+
+      # Beregner satser og uttelling for indikatorer med lukket ramme
+      dplyr::group_by(.data$budsjettar, .data$indikator) %>%
+      dplyr::mutate(sats_rb =
+      dplyr::case_when(is.na(.data$sats_rb) ~
+      .data$ramme / sum(.data$indikatorverdi),
+      TRUE ~ .data$sats_rb))%>%
+      dplyr::group_by(.data$institusjonskode,
+                      .data$indikator,
+                      .data$kategori,
+                      .data$kandidatgruppe,
+                      .data$faktor) %>%
+      dplyr::arrange(.data$budsjettar)%>%
+      dplyr::mutate(budsjettendring =
+      dplyr::coalesce(.data$budsjettendring,
+      (.data$indikatorverdi * .data$sats_rb) -
+      tidyr::replace_na(lag(.data$uttelling_prisjustert), 0))) %>%
+      dplyr::group_by(.data$budsjettar,
+                      .data$institusjonskode_nyeste,
+                      .data$institusjonsnavn_nyeste,
+                      .data$kortnavn_nyeste,
+                      .data$indikator) %>%
+      dplyr::summarise(budsjettendring_urund = sum(.data$budsjettendring)) %>%
+
+      dplyr::mutate(budsjettendring = .round_afz(.data$budsjettendring_urund, -3)) %>%
+      dplyr::mutate(budsjettendring = budsjettendring / 1000)
 
 }
 
