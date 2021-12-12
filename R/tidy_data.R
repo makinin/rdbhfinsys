361159f3fce5e503d73e1b710e278439baa1a6cf
Marija Ninic <57001187+makinin@users.noreply.github.com>
2021-12-12 23:23:19 +0100

    tidy_data commit

--- R/tidy_data.R
+++ R/tidy_data.R
@@ -15,10 +15,10 @@
 
   finsys_dbh$institusjoner %>%
 
-    dplyr::rename(institusjonskode_nyeste =
-                    enc2utf8("institusjonskode (sammensl\u00e5tt)"),
-                  institusjonsnavn_nyeste =
-                    enc2utf8("sammensl\u00e5tt navn"))
+  dplyr::rename(institusjonskode_nyeste =
+                enc2utf8("institusjonskode (sammensl\u00e5tt)"),
+                institusjonsnavn_nyeste =
+                enc2utf8("sammensl\u00e5tt navn"))
 
 }
 
@@ -40,17 +40,17 @@
 .finsys_dbh_studiepoeng <- function(.data, finsys_dbh) {
   finsys_dbh$studiepoeng %>%
     dplyr::rename(indikatorverdi =
-                    .data$`ny produksjon egentfin`) %>%
+    .data$`ny produksjon egentfin`) %>%
     # Bruker finansieringskategorien basert på studentens
     #tilhørighet for BI og for emnet ellers
     dplyr::mutate(kategori =
-                    dplyr::case_when(institusjonskode == "8241" ~
-                                       .data$`finmodkode student`,
-                                     TRUE ~
+      dplyr::case_when(.data$institusjonskode == "8241" ~
+        .data$`finmodkode student`,
+              TRUE ~
                                        .data$`finmodekode emne`)) %>%
     dplyr::mutate(dplyr::across(c("studentkategori", "kategori"),
                                 stringr::str_to_upper)) %>%
-    dplyr::filter(kategori %in% LETTERS[1:6],
+    dplyr::filter(.data$kategori %in% LETTERS[1:6],
                   .data$studentkategori == "S")
 
 }
@@ -108,7 +108,7 @@
 
 .finsys_dbh_utveksling <- function(.data, finsys_dbh) {
   finsys_dbh$utveksling %>%
-    dplyr::rename(indikatorverdi = `antall totalt`) %>%
+    dplyr::rename(indikatorverdi =.data$`antall totalt`) %>%
 
     dplyr::mutate(dplyr::across(c("utvekslingsavtale", "type", "niv\u00e5kode"),
                                 stringr::str_to_upper)) %>%
@@ -118,7 +118,7 @@
                                      ~ "Erasmus+",
                                      utvekslingsavtale != "INDIVID"
                                      ~ "ordinar")) %>%
-    tidyr::drop_na(kategori)
+    tidyr::drop_na(.data$kategori)
 }
 
 #' DBH_API doktorgrader
@@ -135,14 +135,14 @@
 #'
 .finsys_dbh_doktorgarder <- function(.data, finsys_dbh) {
   list(ordinar = finsys_dbh$doktorgrader,
-       `samarbeids-ph.d.` = finsys_dbh$doktorgrader_samarbeid,
-       PKU = finsys_dbh$PKU) %>%
+  `samarbeids-ph.d.` = finsys_dbh$doktorgrader_samarbeid,
+    PKU = finsys_dbh$PKU) %>%
     purrr::map_dfr(~stats::setNames(., stringr::str_replace_all(names(.),
-                                  c("antall($| totalt)" = "indikatorverdi",
-                                 "institusjonskode.*" = "institusjonskode"))),
+                  c("antall($| totalt)" = "indikatorverdi",
+                     "institusjonskode.*" = "institusjonskode"))),
                    .id = "kandidatgruppe") %>%
     dplyr::mutate(faktor = dplyr::case_when(kandidatgruppe ==
-                                     "samarbeids-ph.d." ~ 0.2))
+            "samarbeids-ph.d." ~ 0.2))
 
 }
 
@@ -173,23 +173,24 @@
   finsys_dbh$ekonomi %>%
     dplyr::rename(EU = .data$eu) %>%
     dplyr::mutate(NFR =
-               purrr::map2_dbl(.data$nfr, .data$rff, sum, na.rm = TRUE),
-                  BOA = purrr::map2_dbl(.data$bidragsinntekter,
-                   .data$oppdragsinntekter, sum, na.rm = TRUE))
+    purrr::map2_dbl(.data$nfr, .data$rff, sum, na.rm = TRUE),
+    BOA = purrr::map2_dbl(.data$bidragsinntekter,
+    .data$oppdragsinntekter, sum, na.rm = TRUE))
 
 
 }
 #' Prisjustering
 #' @description Hjep funksjon for beregning av årsdeflator
-#'
+#' @param .data a tibble
 #' @return a tibble
 #' @importFrom dplyr lead
 #' @export
+#' @keywords internal
 
-finsys_dbh_prisjustering <- function() {
-  prisjustering <- prisjustering %>%
-    dplyr::arrange(budsjettar) %>%
-    dplyr::mutate(arsdeflator = 1 / (dplyr::lead(arsendring_pst) / 100 + 1))
+.finsys_dbh_prisjustering <- function(.data) {
+  .finsys_dbh_arsendring() %>%
+  dplyr::arrange(.data$budsjettar) %>%
+  dplyr::mutate(arsdeflator = 1 / (dplyr::lead(.data$arsendring_pst) / 100 + 1))
 }
 
 #' Lager samletabell og beregner endringer og uttelling
@@ -239,7 +240,6 @@ finsys_dbh_tidy <- function(arstall, .data) {
   finsys$ekonomi <- .finsys_dbh_ekonomi(finsys_dbh = finsys_dbh)
 
   finsys$institusjoner <- .finsys_dbh_institutions(finsys_dbh = finsys_dbh)
-  prisjustering <- finsys_dbh_prisjustering()
 
   finsys <-
     c("EU", "NFR", "BOA") %>%
@@ -288,62 +288,22 @@ finsys_dbh_tidy <- function(arstall, .data) {
       .data$faktor) %>%
     dplyr::arrange(.data$budsjettar) %>%
     dplyr::mutate(indikatorendring = .data$indikatorverdi -
-                    dplyr::lag(.data$indikatorverdi)) %>%
-
-    dplyr::anti_join(unntak,
-                     by = c("institusjonskode", "budsjettar", "indikator")) %>%
-    # Legger til satser og beregner budsjettendring
-    #for indikatorer med åpen ramme
-    dplyr::left_join(indikatorsatser,
-                     by = c("budsjettar", "indikator", "kategori")) %>%
-    dplyr::left_join(tilskuddsgrad,
-                     by = c("institusjonskode", "budsjettar", "indikator")) %>%
-
-    dplyr::mutate(budsjettendring =
-                    .data$indikatorendring *
-                    .data$sats_rb *
-                    tidyr::replace_na(.data$faktor, 1) *
-                    tidyr::replace_na(.data$tilskuddsgrad, 1)) %>%
-    # Legger til historisk uttelling for indikatorer med lukket ramme
-    dplyr::left_join(dplyr::left_join(dplyr::filter(uttelling_historisk,
-                                             indikator %in% c("publisering", "BOA", "EU", "NFR")),
-                                      prisjustering,
-                                      by = "budsjettar") %>%
-                       dplyr::mutate(uttelling_prisjustert = uttelling / arsdeflator) %>%
-                       dplyr::select(.data$budsjettar,
-                                     .data$institusjonskode, .data$indikator, .data$uttelling_prisjustert),
-                     by = c("budsjettar", "institusjonskode", "indikator")) %>%
-
-    # Beregner satser og uttelling for indikatorer med lukket ramme
-    dplyr::group_by(.data$budsjettar, .data$indikator) %>%
-    dplyr::mutate(sats_rb =
-                    dplyr::case_when(is.na(.data$sats_rb) ~
-                                       .data$ramme / sum(.data$indikatorverdi),
-                                     TRUE ~ .data$sats_rb))%>%
-    dplyr::group_by(.data$institusjonskode,
-                    .data$indikator,
-                    .data$kategori,
-                    .data$kandidatgruppe,
-                    .data$faktor) %>%
-    dplyr::arrange(.data$budsjettar)%>%
-    dplyr::mutate(budsjettendring =
-                    dplyr::coalesce(.data$budsjettendring,
-                                    (.data$indikatorverdi * .data$sats_rb) -
-                                      tidyr::replace_na(lag(.data$uttelling_prisjustert), 0)
-                                     )
-                  )
+                    dplyr::lag(.data$indikatorverdi))
   finsys$institusjoner <- finsys$institusjoner %>%
 
     dplyr::semi_join(finsys_data, by = "institusjonskode") %>%
     dplyr::left_join(dplyr::select(finsys$institusjoner,
-                                   institusjonskode, kortnavn_nyeste = kortnavn),
-                     by = c("institusjonskode_nyeste" = "institusjonskode"))
+    .data$institusjonskode, kortnavn_nyeste = .data$kortnavn),
+    by = c("institusjonskode_nyeste" = "institusjonskode"))
 
   finsys_data <- finsys_data %>%
     dplyr::left_join(dplyr::select(finsys$institusjoner,
-                                   institusjonskode,
-                                   institusjonskode_nyeste, institusjonsnavn_nyeste, kortnavn_nyeste),
-                     by = "institusjonskode")
+    .data$institusjonskode,
+    .data$institusjonskode_nyeste,
+    .data$institusjonsnavn_nyeste,
+    .data$kortnavn_nyeste),
+    by = "institusjonskode") %>%
+    dplyr::filter(budsjettar == arstall + 2)
 
 
   finsys_data
